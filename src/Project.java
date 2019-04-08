858489d765a59b2332e44118b03992e70ec03fcf
alies <alies@dhcp-39-4-133.dhcpint.csulb.edu>
2019-04-08 15:39:41 -0700

    wip

---
+++ src/Project.java
@@ -0,0 +1,18 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Project {
+	String title;
+	String description;
+	int projectId;
+
+	Project(String title, String description, int projectId) {
+		this.title = title;
+		this.description = description;
+		this.projectId = projectId;
+	}
+
+	Project(ResultSet result) throws SQLException {
+		this(result.getString(1), result.getString(2), result.getInt(3));
+	}
+}
