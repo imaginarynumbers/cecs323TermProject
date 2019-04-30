e5892a1aacf3ec201c7a3c127a107d2724177519
Brandon Mitchell <brandonimitch@users.noreply.github.com>
2019-04-30 15:33:24 -0700

    Exception handling, comments

--- DatabaseObjects/ScrumMember.java
+++ DatabaseObjects/ScrumMember.java
@@ -2,6 +2,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+
+/*
+ * 
+ * ScrumMember class, allows creation, manipulation, and access to ScrumMember objects. Implements the IDatabase Interface.
+ */
 public class ScrumMember implements IDatabaseObject {
     private int scrumId;
     private int employeeId;
@@ -16,35 +21,65 @@ public class ScrumMember implements IDatabaseObject {
         this.employee = employee;
     }
 
-    public ScrumMember(ResultSet result) throws SQLException {
+    public ScrumMember(ResultSet result) throws SQLException {  
         this(result.getInt(1), result.getInt(2));
     }
 
+    /*
+     * Overrides IDatabase Interface method print().
+     * Prints the scrum id number and the employee id number.
+     */
     @Override
     public void print() {
         System.out.println(this.scrumId + " \t" + this.employeeId);
     }
 
+    /*
+     * Overrides IDatabase Interface method getTitle().
+     * Returns the title of the scrum as a string.
+     */
     @Override
     public String getTitle() {
         return null;
     }
 
+    /*
+     * Overrides IDatabase Interface method insert().
+     * Creates and inserts a new scrum member object into the database.
+     * @param db: database object to be manipulated.
+     */
     @Override
     public void insert(Database db) throws SQLException {
         String query = "INSERT INTO ScrumMember (scrumId, employeeId) VALUES (?, ?)";
-        PreparedStatement ps = db.con.prepareStatement(query);
-        ps.setInt(1, this.scrumId);
-        ps.setInt(2, this.employeeId);
-        ps.execute();
+        
+        try {
+            PreparedStatement ps = db.con.prepareStatement(query);
+            ps.setInt(1, this.scrumId);
+            ps.setInt(2, this.employeeId);
+            ps.execute();
+            
+        } catch (SQLException e) {
+        	e.printStackTrace();
+        }
     }
 
+    /*
+     * Overrides IDatabase Interface method delete().
+     * Deletes scrum member objects from database.
+     * @param db: database object to be manipulated.
+     */
     @Override
     public void delete(Database db) throws SQLException {
         String query = "DELETE FROM ScrumMember WHERE scrumId = (?) AND employeeId= (?)";
-        PreparedStatement ps = db.con.prepareStatement(query);
-        ps.setInt(1, this.scrumId);
-        ps.setInt(2, this.employeeId);
-        ps.execute();
+       
+        try {
+            PreparedStatement ps = db.con.prepareStatement(query);
+            ps.setInt(1, this.scrumId);
+            ps.setInt(2, this.employeeId);
+            ps.execute();
+            
+        } catch (SQLException e) {
+        	e.printStackTrace();
+        }
     }
 }
