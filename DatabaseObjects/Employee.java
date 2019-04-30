5863ee475ab3a87db0b1610b88b7b1626a12be38
Brandon Mitchell <brandonimitch@users.noreply.github.com>
2019-04-30 14:44:41 -0700

    Exception handling, comments

--- DatabaseObjects/Employee.java
+++ DatabaseObjects/Employee.java
@@ -3,12 +3,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/*
+ * Employee class, allows manipulation of and access to employee objects. Implements the IDatabase Interface.
+ */
 public class Employee implements IDatabaseObject {
     int employeeId;
     String fName;
     String lName;
     String jobTitle;
 
+    
     Employee(int employeeId, String fName, String lName, String jobTitle) {
         this.employeeId = employeeId;
         this.fName = fName;
@@ -21,39 +25,68 @@ public class Employee implements IDatabaseObject {
                 resultSet.getString(3), resultSet.getString(4));
     }
 
-
+    /*
+     * Overrides IDatabase Interface method delete.
+     * Prints employee information.
+     */
     @Override
     public void print() {
         System.out.println(this.employeeId + "\t" + this.fName + "\t" + this.lName + "\t" + this.jobTitle);
     }
 
+    /*
+     * Overrides IDatabase Interface method delete.
+     * Returns the employee name in a string.
+     */
     @Override
     public String getTitle() {
         return this.fName + " " + this.lName;
     }
 
+    /*
+     * Overrides IDatabase Interface method delete.
+     * Inserts new emlpoyee into database.
+     * @param db: database object to be manipulated.
+     */
     @Override
     public void insert(Database db) throws SQLException {
         String query = "INSERT INTO Employee (fname, lname, jobTitle) " +
                 "VALUES (?, ?, ?)";
-        PreparedStatement ps = db.con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-        ps.setString(1, this.fName);
-        ps.setString(2, this.lName);
-        ps.setString(3, this.jobTitle);
-        ps.execute();
-        ResultSet keys = ps.getGeneratedKeys();
-        if (keys.next()) {
-            this.employeeId = keys.getInt(1);
-        } else {
-            System.err.println("Did not get generated keys: " + query);
+        try {
+            PreparedStatement ps = db.con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, this.fName);
+            ps.setString(2, this.lName);
+            ps.setString(3, this.jobTitle);
+            ps.execute();
+            ResultSet keys = ps.getGeneratedKeys();
+            if (keys.next()) {
+                this.employeeId = keys.getInt(1);
+            } else {
+                System.err.println("Did not get generated keys: " + query);
+            }
+            
+        } catch (SQLException e) {
+        	e.printStackTrace();
         }
+
     }
 
+    /*
+     * Overrides IDatabase Interface method delete.
+     * Deletes employees from database.
+     */
 	@Override
 	public void delete(Database db) throws SQLException {
 		String query = "DELETE FROM Employee WHERE employeeId = ?;";
-        PreparedStatement ps = db.con.prepareStatement(query);
-        ps.setInt(1, this.employeeId);
-        ps.execute();
+		
+		try {
+	        PreparedStatement ps = db.con.prepareStatement(query);
+	        ps.setInt(1, this.employeeId);
+	        ps.execute();
+	        
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
