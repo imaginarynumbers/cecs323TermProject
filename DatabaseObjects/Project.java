61232b4e2a4720df285a673724febdfcddce2ec0
Brandon Mitchell <brandonimitch@users.noreply.github.com>
2019-04-30 15:19:03 -0700

    Exception handling, comments

--- DatabaseObjects/Project.java
+++ DatabaseObjects/Project.java
@@ -3,6 +3,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/*
+ * 
+ * Project class, allows manipulation of and access to project objects. Implements the IDatabase Interface.
+ */
 public class Project implements IDatabaseObject {
 	int projectId;
 	String title;
@@ -16,40 +20,70 @@ public class Project implements IDatabaseObject {
 		
 	}
 
+	
 	Project(ResultSet result) throws SQLException {
 		this(result.getInt(1), result.getString(2), result.getString(3));
 	}
 
+	/*
+     * Overrides IDatabase Interface method print().
+     * Prints project id, title, and description.
+	 */
 	@Override
 	public void print() {
 		System.out.println(this.projectId + " \t" + this.title + "\t\"" + this.description + "\"");
 	}
 
+	/*
+	 * Overrides IDatabase Interface method insert().
+	 * Inserts a new project into database.
+	 * @param db: database object to be manipulated.
+	 */
 	@Override
 	public void insert(Database db) throws SQLException {
 		String query = "INSERT INTO Project (title, description)"
 				+ " VALUES (?, ?)";
-		PreparedStatement ps = db.con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-		ps.setString(1, this.title);
-		ps.setString(2, this.description);
-		ps.execute();
-		ResultSet keys = ps.getGeneratedKeys();
-		if (keys.next())
-			this.projectId = keys.getInt(1);
-		else
-			System.err.println("Did not get generated key: " + query);
+		try {
+			PreparedStatement ps = db.con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1, this.title);
+			ps.setString(2, this.description);
+			ps.execute();
+			ResultSet keys = ps.getGeneratedKeys();
+			if (keys.next())
+				this.projectId = keys.getInt(1);
+			else
+				System.err.println("Did not get generated key: " + query);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
 
+	/*
+	 * Overrides IDatabase Interface method getTitle().
+	 * Returns the project title in a string.
+	 */
 	@Override
 	public String getTitle() {
 		return this.title + " " + this.description;
 	}
 	
+	/*
+	 * Overrides IDatabase Interface method getTitle().
+	 * Deletes projects from database.
+	 * @param db: database object to be manipulated.
+	 */
 	@Override
 	public void delete(Database db) throws SQLException {
 		String query = "DELETE FROM Project WHERE projectId = ?;";
-        PreparedStatement ps = db.con.prepareStatement(query);
-        ps.setInt(1, this.projectId);
-        ps.execute();
+		try {
+	        PreparedStatement ps = db.con.prepareStatement(query);
+	        ps.setInt(1, this.projectId);
+	        ps.execute();
+	        
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
