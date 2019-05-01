c3f2dffebbe1520aca6408b08af6aaa98e0b8329
alies <alies@dhcp-39-122-92.dhcpint.csulb.edu>
2019-04-30 17:17:33 -0700

    rollback sql exception changes

---
+++ old/src/Main.java
@@ -0,0 +1,32 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		String db_url = "jdbc:mysql://cecs-db01.coe.csulb.edu:3306?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
+		Database db = new Database(db_url);
+
+		String user = "cecs323sec7s12";
+		String password = "Veiwai";
+
+		try {
+			db.connect(user, password);
+			db.executeQuery("USE cecs323sec7s12;");
+
+			Controller con = new Controller(db);
+			con.start();
+			/*
+			 * ResultSet res = db.executeQuery("SELECT * FROM Project;"); while (res.next())
+			 * { Project pro = new Project(res); pro.print(); }
+			 */
+			db.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ old/src/State.java
@@ -0,0 +1,16 @@
+import java.sql.SQLException;
+import java.util.Scanner;
+
+abstract class State {
+	Controller controller;
+	MyScanner scan;
+	Database db;
+
+	void setup(Controller con, MyScanner scan, Database db) {
+		this.controller = con;
+		this.scan = scan;
+		this.db = db;
+	}
+
+	abstract State update() throws SQLException;
+}
